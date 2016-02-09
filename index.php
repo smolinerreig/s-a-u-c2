9c7a92824210ee6fe8d2fc21369bd6fc0a8a6f29
niptim <s.moliner.reig@gmail.com>
2016-02-09 17:35:31 +0100

    añadiendo indicador de instancia

--- index.php
+++ index.php
@@ -2,15 +2,16 @@
 <html>
 <head>
 	<meta charset="UTF-8">
+	<script type="text/javascript">
+		document.cookie = ARRAffinity + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+	</script>
 </head> 
 <body>
 <p style="text-align: right; font-size: 0.8em">Instancia: <?php echo $_SERVER["WEBSITE_INSTANCE_ID"]?></p>
 <h1>Icono del día</h1>
 
 <img src="randomicon.php"/>
-<script type="text/javascript">
-	document.cookie = ARRAffinity + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
-</script>
+
 </body>
 </html>
 
