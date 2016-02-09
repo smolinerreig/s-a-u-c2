4778bf82ddfed629c84f5585c26016657e7797ac
niptim <s.moliner.reig@gmail.com>
2016-02-09 16:24:18 +0100

    añadiendo indicador de instancia

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 	<meta charset="UTF-8">
+	<?php headers.Add("Arr-Disable-Session-Affinity", "True"); ?>
 </head> 
 <body>
 <p style="text-align: right; font-size: 0.8em">Instancia: <?php echo $_SERVER["WEBSITE_INSTANCE_ID"]?></p>
